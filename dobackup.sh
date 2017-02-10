0b428e3a56e16053a77ea05230d36dda07de2bff
peterrus <petorrus@gmail.com>
2017-02-11 00:59:08 +0100

    added some more verbosity

--- dobackup.sh
+++ dobackup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo "creating archive"
 tar -zcvf /tmp/$BACKUP_NAME-`date "+%y-%m-%d_%H-%M-%S"`.tar.gz /data
+echo "uploading archive to S3"
 aws s3 cp /tmp/*.tar.gz $S3_BUCKET_URL
+echo "removing local archive"
 rm /tmp/*.tar.gz
+echo "done"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "creating crontab"
 echo "$CRON_SCHEDULE /dobackup.sh" > /etc/crontabs/root
+echo "starting crond"
 crond -f
