2dca8d3b7ae40aebb34f0089ed98efbfabd0cc6d
peterrus <petorrus@gmail.com>
2017-02-11 21:30:33 +0100

    fixed timestamp year pattern (now correctly rendering full year instead of just the last two digits)

--- dobackup.sh
+++ dobackup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "creating archive"
-tar -zcvf /tmp/$BACKUP_NAME-`date "+%y-%m-%d_%H-%M-%S"`.tar.gz /data
+tar -zcvf /tmp/$BACKUP_NAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz /data
 echo "uploading archive to S3"
 aws s3 cp /tmp/*.tar.gz $S3_BUCKET_URL
 echo "removing local archive"
