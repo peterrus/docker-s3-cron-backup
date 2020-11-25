d7de3b029eb14f62103a1b1e060ff441d94bc127
Stefano Blasco <stefano.blasco@ims-evolve.com>
2020-11-25 12:27:07 +0000

    Added TARGET variable to specify backup destination

--- dobackup.sh
+++ dobackup.sh
@@ -6,8 +6,17 @@ S3_STORAGE_CLASS=${S3_STORAGE_CLASS:-STANDARD}
 # generate file name for tar
 FILE_NAME=/tmp/$BACKUP_NAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz
 
+# Check if TARGET variable is set
+if [[ -z ${TARGET} ]];
+then
+    echo "variable TARGET is not set we use default value"
+    TARGET=/data
+else
+    echo "variable TARGET is set"
+fi
+
 echo "creating archive"
-tar -zcvf $FILE_NAME /data
+tar -zcvf $FILE_NAME $TARGET
 echo "uploading archive to S3 [$FILE_NAME, storage class - $S3_STORAGE_CLASS]"
 aws s3 cp --storage-class $S3_STORAGE_CLASS $FILE_NAME $S3_BUCKET_URL
 echo "removing local archive"
